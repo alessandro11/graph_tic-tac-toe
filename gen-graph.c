f47891dd6856fd3ceacd1c1d902c9e184a0b2785
Alessandro Elias <ae11@c3sl.ufpr.br>
2017-03-11 12:08:39 -0300

    Finalized and debuged check_winner().
    Signed-off-by: Alessandro Elias <ae11@c3sl.ufpr.br>

--- gen-graph.c
+++ gen-graph.c
@@ -17,7 +17,7 @@
  */
 
 #include <stdio.h>
-
+#include <string.h>
 
 #define INITIAL_STATE		"O.X......"
 
@@ -34,6 +34,8 @@
 #define	EMPTY		'.'
 
 void gen_graph(const char*);
+int check_winner(int, const char*);
+
 
 /* __________________________________________________________________________________ */
 int main(int argn, char *argv[]) {
@@ -45,11 +47,11 @@ int main(int argn, char *argv[]) {
 }
 
 void gen_graph(const char *init_state) {
-	int stop;
 	char state[16] = {'\0'};
-	int player = 1;
+	(void)(state);
 
-	strncpy(state, init_state, strlen(init_state));
+	//strncpy(state, init_state, strlen(init_state));
+	/*
 	stop = FALSE;
 	do {
 		dot = strchr(EMPTY);
@@ -57,47 +59,114 @@ void gen_graph(const char *init_state) {
 		printf("%s --> %s\n", init_state, state);
 	}
 	while( stop );
+	 */
 }
 
 int check_winner(int player_last_pos, const char *state)
 {
+	int sum = 0;
+
     switch( player_last_pos ) {
         case 0:
-            sum = *state + *(state+1) + *(state+2)
+            sum = *state + *(state+1) + *(state+2);
             if( sum == 237 )
-                return TRUE
-            sum = *state + *(state+4) + *(state+8)
+                return TRUE;
+            sum = *state + *(state+4) + *(state+8);
             if( sum == 237 )
-                return TRUE
-            sum = *state + *(state+3) + *(state+6)
+                return TRUE;
+            sum = *state + *(state+3) + *(state+6);
             if( sum == 237 )
-                return TRUE
+                return TRUE;
         break;
 
         case 1:
-            sum = *(state) + *(state+1) + *(state+2)
+            sum = *(state+1) + *(state) + *(state+2);
             if( sum == 237 )
-                return TRUE
-            sum = *(state+1) + *(state+4) + *(state+7)
+                return TRUE;
+            sum = *(state+1) + *(state+4) + *(state+7);
             if( sum == 237 )
-                return TRUE
+                return TRUE;
 
         break;
 
         case 2:
+            sum = *(state+2) + *(state+1) + *(state);
+            if( sum == 237 )
+                return TRUE;
+            sum = *(state+2) + *(state+4) + *(state+6);
+            if( sum == 237 )
+                return TRUE;
+            sum = *(state+2) + *(state+5) + *(state+8);
+            if( sum == 237 )
+                return TRUE;
         break;
+
         case 3:
+            sum = *(state+3) + *(state) + *(state+6);
+            if( sum == 237 )
+                return TRUE;
+            sum = *(state+3) + *(state+4) + *(state+5);
+            if( sum == 237 )
+                return TRUE;
         break;
+
         case 4:
+            sum = *(state+4) + *(state) + *(state+8);
+            if( sum == 237 )
+                return TRUE;
+            sum = *(state+4) + *(state+1) + *(state+7);
+            if( sum == 237 )
+                return TRUE;
+            sum = *(state+4) + *(state+2) + *(state+6);
+            if( sum == 237 )
+                return TRUE;
+            sum = *(state+4) + *(state+3) + *(state+5);
+            if( sum == 237 )
+                return TRUE;
         break;
+
         case 5:
+            sum = *(state+5) + *(state+4) + *(state+6);
+            if( sum == 237 )
+                return TRUE;
+            sum = *(state+5) + *(state+2) + *(state+8);
+            if( sum == 237 )
+                return TRUE;
         break;
+
         case 6:
+            sum = *(state+6) + *(state+3) + *(state);
+            if( sum == 237 )
+                return TRUE;
+            sum = *(state+6) + *(state+4) + *(state+2);
+            if( sum == 237 )
+                return TRUE;
+            sum = *(state+6) + *(state+7) + *(state+8);
+            if( sum == 237 )
+                return TRUE;
         break;
+
         case 7:
+            sum = *(state+7) + *(state+6) + *(state+8);
+            if( sum == 237 )
+                return TRUE;
+            sum = *(state+7) + *(state+1) + *(state+4);
+            if( sum == 237 )
+                return TRUE;
         break;
+
         case 8:
+            sum = *(state+8) + *(state+2) + *(state+5);
+            if( sum == 237 )
+                return TRUE;
+            sum = *(state+8) + *(state) + *(state+4);
+            if( sum == 237 )
+                return TRUE;
+            sum = *(state+8) + *(state+6) + *(state+7);
+            if( sum == 237 )
+                return TRUE;
         break;
     }
 
+	return FALSE;
 }
